2ef2fe8635b1de1198cd143df404b7772edb9b03
Bnahian <bnahian@gmail.com>
2022-08-06 12:23:47 -0500

    created read me function

--- Develop/index.js
+++ Develop/index.js
@@ -1,21 +1,38 @@
 // TODO: Include packages needed for this application
-const fs = require('fs');
-const inquirer = require("inquirer");
-const chalk = require('chalk')
-
-
-
-
-
-
+const inquirer = require('inquirer');
+const fs= require('fs');
 // TODO: Create an array of questions for user input
-const questions = [];
-
+inquirer
+ .prompt([
+    {
+        type: 'input',
+        name:'motivation',
+        message: 'What was your motivation?',
+    },
+    {
+        type:'input',
+        name: 'build',
+        message: 'Why did you build this project?',
+    },
+    {
+        type: 'input',
+        name: 'solve',
+        message: 'What problems does it solve?'
+    },
+    {
+        type: 'input',
+        name: 'learn',
+        message: 'What did you learn?',
+    }
+])
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+  .then((data) => {
+    const filename = `${data.who.toLowerCase().split(' ').join('')}.json`;
+console.log(data)
+    fs.writeFile(filename, JSON.stringify(data, null, '\t'), (err) =>
+      err ? console.log(err) : console.log('Success!')
+    );
+  });
 
-// TODO: Create a function to initialize app
-function init() {}
 
-// Function call to initialize app
-init();
+
